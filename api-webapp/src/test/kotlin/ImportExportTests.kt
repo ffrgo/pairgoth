5ec283c05e956d631eecaef64bf32d7b5526b61a
Theo Barollet <theo.barollet@inria.fr>
2023-07-08 17:56:25 +0200

    added import export test

--- api-webapp/src/test/kotlin/ImportExportTests.kt
+++ api-webapp/src/test/kotlin/ImportExportTests.kt
@@ -1,5 +1,7 @@
 package org.jeudego.pairgoth.test
 
+import org.jeudego.pairgoth.ext.OpenGotha
+import org.jeudego.pairgoth.util.XmlUtils
 import org.junit.jupiter.api.Test
 import java.nio.charset.StandardCharsets
 
@@ -22,6 +24,21 @@ class ImportExportTests: TestBase() {
             }
             val xml = TestAPI.getXml("/api/tour/$id")
             logger.info(xml.slice(0..50)+"...")
+        }
+    }
+
+    @Test
+    fun `002 test opengotha import export`() {
+        // We import a tournament
+        // Check that after exporting and reimporting we get the same pairgoth tournament object
+        getTestResources("opengotha").forEach { file ->
+            val resource = file.readText(StandardCharsets.UTF_8)
+            val root_xml = XmlUtils.parse(resource)
+            val tournament = OpenGotha.import(root_xml)
+
+            val exported = OpenGotha.export(tournament)
+            val tournament2 = OpenGotha.import(XmlUtils.parse(exported))
+            assert(tournament == tournament2)
         }
     }
 }
