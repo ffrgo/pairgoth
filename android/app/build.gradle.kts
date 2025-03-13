3c7cfc831476ec2a60ae956eedcb44b74fe0ba2c
lgiulian <iuliancl2000@yahoo.com>
2025-03-13 16:57:03 +0200

    improve the look a bit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
